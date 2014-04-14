b24037e4c17cbe8cf88a7ea742a1cea8edb58c07
Trevor Helms <thelms501@gmail.com>
2014-04-14 19:56:14 -0400

    Add static directory access 'public/'

--- app.js
+++ app.js
@@ -2,8 +2,15 @@
 var express = require('express');
 var app = express();
 
+// Allow files in public/ to be accessed directly
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+	res.send("It's working...");
+});
+
 
 // Boot up server
-var server = app.lister(3000. function() {
+var server = app.listen(3000. function() {
 	console.log('Listening on port %d', server.address().port);
 });
