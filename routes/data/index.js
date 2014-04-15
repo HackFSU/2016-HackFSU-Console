11eb4f4ae5f9eed57226cfc34f6195f0b6fb2c2c
Trevor Helms <thelms501@gmail.com>
2014-04-15 18:00:29 -0400

    Add kaiseki library for Parse

--- routes/data/index.js
+++ routes/data/index.js
@@ -1,3 +1,5 @@
+require('../../src/init.js');
+
 exports.index = function(req, res) {
 	res.render('data/index', {
 		title: 'Data Management'
---
+++ src/init.js
@@ -0,0 +1,6 @@
+var Kaiseki = require('kaiseki');
+var APP_ID = "4NDzxeC8KxdZi4Kyok7QfGhtS27GuHfntNh9ZSfL";
+var MASTER_KEY = "k5K40usqxTLInr0OkDpyanoFO6ChaDkQsZTfCwRu";
+var REST_KEY = "Yv6wS2RcB2iYqs3Fn7kNpGsjSSquY0Xj50uKQxbFar";
+var kaiseki = new Kaiseki(APP_ID, REST_KEY);
+kaiseki.masterKey = MASTER_KEY;
