5c3574257692331463e1f525b0da76a9832f36f3
Trevor Helms <thelms501@gmail.com>
2014-04-14 21:43:49 -0400

    Add navbar template

--- app.js
+++ app.js
@@ -2,12 +2,18 @@
 var express = require('express');
 var app = express();
 
+// Set up views/templating engine
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
 // Allow files in public/ to be accessed directly
 app.use(express.static(__dirname + '/public'));
 
 
 app.get('/', function(req, res) {
-	res.send("It's working...");
+	res.render('index', {
+		title: 'HackFSU Console'
+	});
 });
 
 
