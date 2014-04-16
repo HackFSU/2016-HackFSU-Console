68b8b991294165aa85328c095eede922915a96ab
Trevor Helms <thelms501@gmail.com>
2014-04-16 10:30:32 -0400

    Add count to users table

--- routes/data/index.js
+++ routes/data/index.js
@@ -2,13 +2,14 @@ var kaiseki = require('../../src/init.js').kaiseki;
 
 exports.index = function(req, res) {
     kaiseki.getUsers({
-            limit: 500
+            limit: 500,
+            count: true
         },
         function(err, result, body, success) {
-            //users = body;
             res.render('data/index', {
                 title: 'Data Management',
-                people: body
+       			count: body.count,
+                users: body.results
             });
         });
 };
