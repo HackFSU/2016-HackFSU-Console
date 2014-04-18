6d6e45b28fa0d6a73a7998d0a77b36c3a11b2c59
Trevor Helms <thelms501@gmail.com>
2014-04-18 13:00:03 -0400

    Improve functionality of the timeAgo function

--- routes/updates/index.js
+++ routes/updates/index.js
@@ -23,6 +23,34 @@ exports.add = function(kaiseki) {
     };
 };
 
+/*
+* Converts a time string into a string representing the "time ago" since then
+* Example: 5 seconds ago, 3 hours ago
+*/
 function timeAgo(time) {
-	return Math.floor((Date.now() - Date.parse(time)) / 1000 / 60 / 60 / 24) + ' days ago';
+	var millis = Date.parse(time);
+	var daysAgo = (Date.now() - millis) / 1000 / 60 / 60 / 24;
+
+	if (daysAgo >= 1) {
+		var ago = (daysAgo >= 2) ? ' days ago' : ' day ago'; 
+		return Math.floor(daysAgo) + ago;
+	}
+
+	var hoursAgo = daysAgo * 24;
+
+	if (hoursAgo >= 1) {
+		var ago = (hoursAgo >= 2) ? ' hours ago' : ' hour ago'; 
+		return Math.floor(hoursAgo) + ago;
+	}
+
+	var minsAgo = hoursAgo * 60;
+
+	if (minsAgo >= 1) {
+		var ago = (minsAgo >= 2) ? ' minutes ago' : ' minute ago'; 
+		return Math.floor(minsAgo) + ago;
+	}
+
+	var secsAgo = minsAgo * 60;
+	var ago = (secsAgo >= 2) ? ' seconds ago' : ' second ago'; 
+	return Math.floor(secsAgo) + ago;
 }
