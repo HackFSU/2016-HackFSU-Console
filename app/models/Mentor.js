d320e7d32387ebdf8f89ba904d8a9c86225bfbf4
Trevor Helms <thelms501@gmail.com>
2015-10-31 16:28:05 -0400

    Add some more Parse models

--- app/models/Mentor.js
+++ app/models/Mentor.js
@@ -1,8 +1,30 @@
 /**
  * Mentor model
  *
- * 
+ *
  */
 'use strict';
 
+export default function (app) {
+    const PARSE_CLASSNAME = 'Mentor';
 
+    const Parse = app.Parse;
+    const _ = app._;
+    const validate = app.validate;
+
+    class Mentor extends Parse.Object {
+        constructor(o) {
+            super(PARSE_CLASSNAME);
+
+            o = validate(o, _.isObject);
+            this.hackathons = validate(o, _.isArray);
+            this.affiliation = validate(o, _.isString);
+            this.availability = validate(o, _.isArray);
+            this.skills = validate(o, _.isArray);
+            this.user = validate();     // How do we validate that this is a pointer to a Parse User?
+        }
+    }
+
+    app.model.Mentor = Mentor;
+    Parse.Object.registerSubclass(PARSE_CLASSNAME, Mentor);
+}
