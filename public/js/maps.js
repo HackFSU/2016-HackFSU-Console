479c5d1e836dea4e70e881d957ad786ed5ad08f9
Remi <rtrettin@vapornode.com>
2015-12-03 21:12:08 -0500

    JS style changes

--- public/js/maps.js
+++ public/js/maps.js
@@ -1,53 +1,60 @@
-var geo = $('#geo');
-var scroll = $('#contact').offset().top;
-var lati = 30.445401;
-var longi = -84.299761;
-var gotPos = false;
+/**
+ * Waits for user to scroll to #contact div then asks for browser geolocation permission.
+ * Uses G Maps API to get/show travel time
+ */
 
-$(window).on('scroll', function() {
-	var ypos = window.pageYOffset;
-	if(ypos > scroll && gotPos === false) {
-		if(navigator.geolocation) {
-			navigator.geolocation.getCurrentPosition(showPosition, showError);
-			gotPos = true;
-		}else{
-			geo.html("Geolocation is not supported by this browser.");
-		}
-	}
-});
+(function($) {
+	var geo = $('#geo');
+	var scroll = $('#contact').offset().top;
+	var lati = 30.445401;
+	var longi = -84.299761;
+	var gotPos = false;
 
-function showPosition(position) {
-	var origin = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-	var destination = new google.maps.LatLng(lati, longi);
-
-	var directionsService = new google.maps.DirectionsService();
-	var request = {
-		origin: origin,
-		destination: destination,
-		travelMode: google.maps.DirectionsTravelMode.DRIVING
-	};
-
-	directionsService.route(request, function(response, status) {
-		if(status === 'OK') {
-			var point = response.routes[0].legs[0];
-			var str = point.duration.text.replace("mins", "minutes");
-			geo.html("You could be here in just " + str + "!");
+	$(window).on('scroll', function() {
+		var ypos = window.pageYOffset;
+		if(ypos > scroll && gotPos === false) {
+			if(navigator.geolocation) {
+				navigator.geolocation.getCurrentPosition(showPosition, showError);
+				gotPos = true;
+			}else{
+				geo.html('Geolocation is not supported by this browser.');
+			}
 		}
 	});
-}
-function showError(error) {
-	switch(error.code) {
-		case error.PERMISSION_DENIED:
-			geo.html("User denied request for geolocation.");
-			break;
-		case error.POSITION_UNAVAILABLE:
-			geo.html("Location information is unavailable.");
-			break;
-		case error.TIMEOUT:
-			geo.html("The request to get user location timed out.");
-			break;
-		case error.UNKNOWN_ERROR:
-			geo.html("An unknown error occurred.");
-			break;
+
+	var showPosition = function(position) {
+		var origin = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+		var destination = new google.maps.LatLng(lati, longi);
+
+		var directionsService = new google.maps.DirectionsService();
+		var request = {
+			origin: origin,
+			destination: destination,
+			travelMode: google.maps.DirectionsTravelMode.DRIVING
+		};
+
+		directionsService.route(request, function(response, status) {
+			if(status === 'OK') {
+				var point = response.routes[0].legs[0];
+				var str = point.duration.text.replace('mins', 'minutes');
+				geo.html('You could be here in just ' + str + '!');
+			}
+		});
 	}
-}
+	var showError = function(error) {
+		switch(error.code) {
+			case error.PERMISSION_DENIED:
+				geo.html('Oh no! You denied the location request.');
+				break;
+			case error.POSITION_UNAVAILABLE:
+				geo.html('Your current location is unavailable.');
+				break;
+			case error.TIMEOUT:
+				geo.html('The location request timed out.');
+				break;
+			case error.UNKNOWN_ERROR:
+				geo.html('An unknown error occurred.');
+				break;
+		}
+	}
+})(jQuery);
