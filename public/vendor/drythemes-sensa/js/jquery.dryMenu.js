0847dec067e3493c44cb8a31f64eafada9dc6ac9
Trevor Helms <thelms501@gmail.com>
2015-12-09 05:53:45 -0500

    Links working

--- public/vendor/drythemes-sensa/js/jquery.dryMenu.js
+++ public/vendor/drythemes-sensa/js/jquery.dryMenu.js
@@ -1,26 +1,27 @@
-$(window).bind('scroll resize', function() {	
+$(window).bind('scroll resize', function() {
     var currentSection = null;
-	
+
     $('.section').each(function(){
-        var element = $(this).attr('id');	
+        var element = $(this).attr('id');
         if ($('#'+element).is('*')){
             if($(window).scrollTop() >= $('#'+element).offset().top - 90)
             {
+								console.log(currentSection);
                 currentSection = element;
             }
         }
     });
-		
-    $('#main-menu ul li').removeClass('active').find('a[href="#'+currentSection+'"]').parent().addClass('active');	
-	
+
+    $('#main-menu ul li').removeClass('active').find('a[href="#'+currentSection+'"]').parent().addClass('active');
+
 	jQuery('select.small-menu option:selected').removeAttr('selected');
 	jQuery('select.small-menu option[value="#'+currentSection+'"]').attr('selected', 'selected');
-	
+
 });
 
-jQuery(document).ready(function() { 
-	$("#main-menu").sticky({ topSpacing: 0 });  	
-    $('#main-menu ul li a, a.slow-scroll').click(function() {	
+jQuery(document).ready(function() {
+	$("#main-menu").sticky({ topSpacing: 0 });
+    $('#main-menu ul li a, a.slow-scroll').click(function() {
         $('html, body').animate({scrollTop: $(this.hash).offset().top}, 2000);
         return false;
     });
