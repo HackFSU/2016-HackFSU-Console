a31cc0f6d4371ee21d7459e09e38f868d0946245
Trevor Helms <thelms501@gmail.com>
2016-01-28 12:31:19 -0500

    Create a method to save a Confirmation
    I'm not exactly sure how to override the save() method and still use the
    Parse SDK to save the data once that function is overwritten. Instead, to
    be safe for now, I simply named the function saveIt() so that there aren't
    any issues. We'll need to come back to that later and fix it.

--- app/models/Confirmation.js
+++ app/models/Confirmation.js
@@ -22,18 +22,35 @@ export default class Confirmation extends Parse.Object {
 
 		validate(o, _.isPlainObject);
 		confirm.set('waiver', validate(o.waiver, _.isBoolean));
-		confirm.set('waiver-signature', validate(o.waiverSignature, _.isString));
+		confirm.set('waiverSignature', validate(o.waiverSignature, _.isString));
 		confirm.set('mlhDataSharing', validate(o.mlhDataSharing, _.isBoolean));
+		confirm.hackerId = validate(o.hackerId, _.isString);
+
+		return confirm;
+	}
+
+	/**
+	* Save the confirmation data. Needs to set the hacker object first.
+	* Idk how to fucking do this properly. This is what I came up with.
+	*/
+	saveIt() {
+		let promiseSave = new Parse.Promise();
 
 		let query = new Parse.Query(Hacker);
-		query.get(o.hackerId).then(function(hacker) {
-			confirm.set('hacker', hacker);
-			return confirm;
+		query.get(this.hackerId).then((hacker) => {
+			console.log(hacker);
+			this.set('hacker', hacker);
+			this.save().then(function(confirm) {
+				promiseSave.resolve(confirm);
+			}, function(err) {
+				promiseSave.reject(err);
+			});
 		}, function(err) {
 			throw new Error(`Error finding the hacker by ID supplied: ${err}`);
 		});
-	}
 
+		return promiseSave;
+	}
 
 }
 
