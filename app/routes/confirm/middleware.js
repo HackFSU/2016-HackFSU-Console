042d859c18894e42037ff26284016c665cfebe01
Trevor Helms <thelms501@gmail.com>
2016-01-28 03:45:34 -0500

    Update the submit confirmation function

--- app/routes/confirm/middleware.js
+++ app/routes/confirm/middleware.js
@@ -10,7 +10,9 @@ import Confirmation from 'app/models/Confirmation';
 
 
 /**
-* Middleware to validate a confirmation submission
+* Middleware to validate a confirmation submission.
+* This is fairly trivial, but necessary. We also set some fields statically,
+* because they are implied from submitting the confirmation form.
 */
 export function validateConfirmation(req, res, next) {
 	req.sanitizeBody('waiverSignature').trim();
@@ -36,8 +38,10 @@ export function validateConfirmation(req, res, next) {
 }
 
 /**
-* Middleware to save the phone number parameter for a hacker if it is set
-* Otherwise we skip along
+* Middleware to save the phone number parameter for a hacker if it is set,
+* since some people didn't enter their number when they registered.
+* And because Trevor forgot to make it a required field :)
+* If not set, we skip along happily to our next destination.
 */
 export function savePhoneIfSet(req, res, next) {
 	if (!_.isEmpty(req.body.phone)) {
@@ -48,11 +52,25 @@ export function savePhoneIfSet(req, res, next) {
 }
 
 /**
-* Middleware to submit a confirmation
-*
-* Stores the confirmation data in a Confirmation object
+* Middleware to submit a confirmation.
+* Stores the confirmation data in a Confirmation object.
+* If we get an error, we send it and don't process the confirmation (which may
+* caused some headaches if ever reached...)
 */
 export function submitConfirmation(req, res, next) {
-	
+	// Pick the attrs we want for a confirmation
+	const confirmAttrs = _.pick(req.body,
+		'waiver',
+		'waiverSignature',
+		'mlhDataSharing',
+		'hackerId'
+	);
+
+	// Create and save the confirmation object
+	Confirmation.new(confirmAttrs).save().then(function(confirmation) {
+		res.json({
+			confirmation: confirmation
+		});
+	});
 
 }
