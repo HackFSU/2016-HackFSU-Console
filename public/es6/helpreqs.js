29e56a12952f6ef0326c25a8535de6a239230c0d
Trevor Helms <thelms501@gmail.com>
2016-01-30 18:23:47 -0500

    Interstitial

--- public/es6/helpreqs.js
+++ public/es6/helpreqs.js
@@ -1,9 +1,15 @@
 /**
- * Help requests. Trever wtf you are so inconsistent in your bad practices.
+ * Help requests.
  */
 
 (function($) {
 	'use strict';
+
+	var socket = io.connect('http://localhost:5003');
+	socket.on('news', function (data) {
+		console.log(data);
+		socket.emit('another event', { my: 'data' });
+	});
 
 	$('#helpreqs').DataTable({
 		paging: false
