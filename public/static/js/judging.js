904b4f045dd8715074470830c498ff6c358b8093
Remi Trettin <rtrettin@vapornode.com>
2016-02-04 23:19:49 -0500

    responsive text, slider fixes

--- public/static/js/judging.js
+++ public/static/js/judging.js
@@ -27,10 +27,6 @@ $('#beginjudge').click(function() {
 	$('#judge2').fadeIn("slow");
 	window.scrollTo(0, 0);
 });
-$('#judgerefresh').click(function() {
-	location.reload();
-});
-
 $('#endjudge').click(function(e) {
 	var form = $('#judgeform');
 	var data = form.serialize();
@@ -52,32 +48,28 @@ $('#endjudge').click(function(e) {
 	e.preventDefault();
 });
 
+$('#judgerefresh').click(function() {
+	location.reload();
+});
+
 noUiSlider.create(jslider1, sliderConfig);
-
 noUiSlider.create(jslider2, sliderConfig);
-
 noUiSlider.create(jslider3, sliderConfig);
 
 jslider1.noUiSlider.on('update', function() {
 	last = jslider1.getAttribute('id');
 	lastTotal = total;
 	total = BASE - (parseInt(jslider1.noUiSlider.get()) + parseInt(jslider2.noUiSlider.get()) + parseInt(jslider3.noUiSlider.get()));
-});
 
-jslider2.noUiSlider.on('update', function() {
-	last = jslider2.getAttribute('id');
-	lastTotal = total;
-	total = BASE - (parseInt(jslider1.noUiSlider.get()) + parseInt(jslider2.noUiSlider.get()) + parseInt(jslider3.noUiSlider.get()));
-});
+	document.getElementById("judgingpoints").innerHTML = total;
 
-jslider3.noUiSlider.on('update', function() {
-	last = jslider3.getAttribute('id');
-	lastTotal = total;
-	total = BASE - (parseInt(jslider1.noUiSlider.get()) + parseInt(jslider2.noUiSlider.get()) + parseInt(jslider3.noUiSlider.get()));
+	if(total < 0) {
+		document.getElementById(last).noUiSlider.set(lastTotal);
+		document.getElementById("judgingpoints").innerHTML = 0;
+	}
 });
 
 jslider1.noUiSlider.on('change', function() {
-	document.getElementById("judgingpoints").innerHTML = total;
 	if(total <= 0) {
 		jslider1.setAttribute("disabled", true);
 		jslider2.setAttribute("disabled", true);
@@ -87,6 +79,15 @@ jslider1.noUiSlider.on('change', function() {
 		jslider2.removeAttribute("disabled");
 		jslider3.removeAttribute("disabled");
 	}
+});
+
+jslider2.noUiSlider.on('update', function() {
+	last = jslider2.getAttribute('id');
+	lastTotal = total;
+	total = BASE - (parseInt(jslider1.noUiSlider.get()) + parseInt(jslider2.noUiSlider.get()) + parseInt(jslider3.noUiSlider.get()));
+
+	document.getElementById("judgingpoints").innerHTML = total;
+
 	if(total < 0) {
 		document.getElementById(last).noUiSlider.set(lastTotal);
 		document.getElementById("judgingpoints").innerHTML = 0;
@@ -94,7 +95,6 @@ jslider1.noUiSlider.on('change', function() {
 });
 
 jslider2.noUiSlider.on('change', function() {
-	document.getElementById("judgingpoints").innerHTML = total;
 	if(total <= 0) {
 		jslider1.setAttribute("disabled", true);
 		jslider2.setAttribute("disabled", true);
@@ -104,6 +104,15 @@ jslider2.noUiSlider.on('change', function() {
 		jslider2.removeAttribute("disabled");
 		jslider3.removeAttribute("disabled");
 	}
+});
+
+jslider3.noUiSlider.on('update', function() {
+	last = jslider3.getAttribute('id');
+	lastTotal = total;
+	total = BASE - (parseInt(jslider1.noUiSlider.get()) + parseInt(jslider2.noUiSlider.get()) + parseInt(jslider3.noUiSlider.get()));
+
+	document.getElementById("judgingpoints").innerHTML = total;
+
 	if(total < 0) {
 		document.getElementById(last).noUiSlider.set(lastTotal);
 		document.getElementById("judgingpoints").innerHTML = 0;
@@ -111,7 +120,6 @@ jslider2.noUiSlider.on('change', function() {
 });
 
 jslider3.noUiSlider.on('change', function() {
-	document.getElementById("judgingpoints").innerHTML = total;
 	if(total <= 0) {
 		jslider1.setAttribute("disabled", true);
 		jslider2.setAttribute("disabled", true);
@@ -120,9 +128,5 @@ jslider3.noUiSlider.on('change', function() {
 		jslider1.removeAttribute("disabled");
 		jslider2.removeAttribute("disabled");
 		jslider3.removeAttribute("disabled");
-	}
-	if(total < 0) {
-		document.getElementById(last).noUiSlider.set(lastTotal);
-		document.getElementById("judgingpoints").innerHTML = 0;
 	}
 });
