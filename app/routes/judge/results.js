ae2981b250bbc67a9de7de9bc78fab002fcd2ac3
Jared Bennett <jrdbnntt@gmail.com>
2016-02-07 09:34:40 -0500

    expo2

--- app/routes/judge/results.js
+++ app/routes/judge/results.js
@@ -3,13 +3,86 @@
  *
  * Handles Judge Administration
  */
+/* jshint -W083 */
+
 'use strict';
 
 import express from 'express';
 import Parse from 'parse/node';
+import { acl, queryFind } from 'app/routes/util';
+import JudgeRound from 'app/models/JudgeRound';
+import store from 'app/store';
 
 const router = express.Router();
 
-// TODO
+router.use(acl.use('Admin'));
+
+
+
+router.route('/list')
+.get(
+	// grab all round data
+	queryFind(function() {
+		let query = new Parse.Query(JudgeRound);
+		query.equalTo('status', 'completed');
+		return query;
+	}),
+
+	// quantify
+	function(req, res, next) {
+		let scores = {
+			top: {}
+		};
+
+		// init the categories
+		store.hackCategories.forEach(function(cname) {
+			scores[cname] = {};
+		});
+
+		function addCount(map, id) {
+			if(id !== 0 && !id) {
+				return;
+			}
+
+			if(!map[id]) {
+				map[id] = 1;
+			} else {
+				map[id] += 1;
+			}
+		}
+
+		// get the counts
+		let jrounds = res.locals.queryResults;
+		jrounds.forEach(function(round) {
+			let points = round.get('points');
+			let noms = round.get('nominations');
+
+			points.forEach(function(id) {
+				addCount(scores.top, id);
+			});
+
+			console.log('noms', noms);
+
+			for(let cname in noms) {
+				if(noms.hasOwnProperty(cname)) {
+					addCount(scores[cname], noms[cname]);
+				}
+			}
+		});
+
+		req.log.info(scores);
+		res.json({
+			scores: scores
+		});
+
+	},
+
+	// get top ten
+
+	// return
+	function(req, res) {
+		res.json({});
+	}
+);
 
 export default router;
